abc3e5741827ecc18f7a5f931dcd6357f669d8ab
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-11-01 23:54:08 +0200

    Added data file

---
+++ client/data/data.js
@@ -0,0 +1,52 @@
+export const developerData = [
+    {
+        id:1,
+        text: 'Besides the fact that SovTech is a software development company. When I did my research about the company, different sources, and employees all say positive things about the company. Weather young or old people treat each other with respect. There is room for improvement and/or growth. That is one of the things I like about SovTech. The research i conducted states that there is a great company culture.'
+    },
+    {
+        id: 2,
+        text: 'My former employer had an open office plan, from the pictures I saw online i can tell that SovTech has an open office plan. Having everyone working within an open space can be great for turning an office into a productive hub of communication and collaboration. It would be a great learning enviroment surrounding myself with everyone in the office.'
+    }
+]
+
+export const hobbiesData = [
+    {
+        id: 1,
+        text: 'Cooking'
+    },
+    {
+        id: 2,
+        text: 'Mountain Hiking'
+    },
+    {
+        id: 3,
+        text: 'Stretching'
+    },
+    {
+        id: 4,
+        text: 'Bowling'
+    },
+    {
+        id: 5,
+        text: 'Cycling'
+    },
+    {
+        id: 6,
+        text: 'designing'
+    },
+]
+
+export const selfData = [
+    {
+        id: 1,
+        text: 'Well I am an introvert, I always try smiling, even though I am sad. I always challange myself through coding and life in general. I spend most of my time coding or practicing maths. I am person who likes to chill with his family and friends.'
+    },
+    {
+        id: 2,
+        text: 'After high school, I furthered my studies in System Development and Software Engineering. I like upskilling myself in the following frameworks and programming languages, React, Node.js, Flask, Javascript, and Python.'
+    },
+    {
+        id: 3,
+        text: 'I like cooking, and Mountain hiking those are some of my hobbies.'
+    }
+]
