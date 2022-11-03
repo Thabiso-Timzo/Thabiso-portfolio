0dd8a1e59c1b0db4544a046c8860266c74868247
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-11-03 18:09:13 +0200

    Updated data.js file

--- Thabiso_project/data/data.js
+++ Thabiso_project/data/data.js
@@ -1,12 +1,17 @@
 export const developerData = [
     {
         id:1,
-        text: 'Besides the fact that SovTech is a software development company. When I did my research about the company, different sources, and employees all say positive things about the company. Weather young or old people treat each other with respect. There is room for improvement and/or growth. That is one of the things I like about SovTech. The research i conducted states that there is a great company culture.'
+        text: 'Besides the fact that SovTech is a software development company. When I did my research about the company, different sources, and employees all say positive things about the company. Whether young or old people treat each other with respect. There is always room for improvement and/or growth. That is one of the things I like about SovTech. The research i conducted states that there is a great company culture.'
     },
     {
         id: 2,
         text: 'My former employer had an open office plan, from the pictures I saw online i can tell that SovTech has an open office plan. Having everyone working within an open space can be great for turning an office into a productive hub of communication and collaboration. It would be a great learning enviroment surrounding myself with everyone in the office.'
     }
+    ,
+    {
+        id: 3,
+        text: " I\’ve always loved to code and designing, since from my days in high school. So I’m excited to see that coding and ux/ui design is a big part of SovTech. Programming has always been fun for me, the challenge of solving problems tells a story and convey an idea. There's so many things i can learn from mentor or SovTech employees."
+    },
 ]
 
 export const hobbiesData = [
@@ -32,21 +37,29 @@ export const hobbiesData = [
     },
     {
         id: 6,
-        text: 'designing'
+        text: 'Designing'
     },
+    {
+        id: 7,
+        text: 'Jogging'
+    },
+    {
+        id: 8,
+        text: 'Walking'
+    }
 ]
 
 export const selfData = [
     {
         id: 1,
-        text: 'Well I am an introvert, I always try smiling, even though I am sad. I always challange myself through coding and life in general. I spend most of my time coding or practicing maths. I am person who likes to chill with his family and friends.'
+        text: "Who is Thabiso Hlatshwayo? Well I am a person who believes working alone is productive but working in a team brings in very good results, and I understand that in order to achieve difficult work or projects or tasks, I should work with other people, I must consider thier skills and experiences. I am also good at working under pressure, I actually prefer to be working under pressure. I tend to be at my best under pressure. I am also good in building up relationships with different people even though at first I am shy and I talk less, so I am always focused on achieving difficult tasks and projects that are aligned to work environments."
     },
     {
         id: 2,
-        text: 'After high school, I furthered my studies in System Development and Software Engineering. I like upskilling myself in the following frameworks and programming languages, React, Node.js, Flask, Javascript, and Python.'
+        text: "I have just over a 6 months of experience as an engineer with first technology. I have already contributed to over a dozen projects and assisted with managing one project for one of the firm\'s long-time clients. I have studied System development and Software engineering after high school. I have learnt to code since grade 11, I used to design a single page website using HTML and CSS, then I learnt JavaScript, Python, Java, and C# in tertiary."
     },
     {
         id: 3,
-        text: 'I like cooking, and Mountain hiking those are some of my hobbies.'
+        text: "During my spare time there\'s a lot of things I like doing, but I'll name a few. I like cooking, event though I'm not yet perfect but that helps me organise everything I do in my life. Hiking, helps me to discover what i love, my love for animals came from hiking, it helped my to excise daily, to try and keep myself feet. Those are a few things about myself. :)"
     }
 ]
